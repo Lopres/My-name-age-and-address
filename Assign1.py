8f9070bb77e099e0ec1e45b7c2bf6193edfe890b
Lopres <latedan23@gmail.com>
2021-10-30 02:31:09 +0800

    First Commit

---
+++ Assign1.py
@@ -0,0 +1,4 @@
+name = input("Input yout First Name: ")
+age = input("Input your Age: ")
+address = input("Input your Address: ")
+print (f"Hello, my name is {name} I am {age} years old and I live in {address}")
